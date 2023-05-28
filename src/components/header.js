ced187f94c8d7ca2a998f27b54213d52a6417712
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-27 21:47:37 -0600

    Restarted the todo app from scratch

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,9 @@
-
-
 export function NewToDoList() {
-  const [todos, setTodos] = useState([])
-
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return ( ...todo, completed)
-        }
-
-        return todo
-      })
-    })
-  }
-
   return (
-    <form onSubmit={handleSubmit} className="new-item-form">
+    <form className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
-        <input
-          value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
-          type="text"
-          id="itme"
-        />
+        <input type="text" id="itme" />
       </div>
     </form>
   );
---
+++ src/components/list.js
@@ -0,0 +1,12 @@
+export function TodoList() {
+  return (
+    <ul className="todo-list">
+      <li>
+        Item 1 <button>Delete</button>
+      </li>
+      <li>
+        Item 2 <button>Delete</button>
+      </li>
+    </ul>
+  );
+}

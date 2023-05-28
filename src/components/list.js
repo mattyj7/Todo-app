e80be23df7f406e2f93173c2827bd711bf5f785a
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-28 14:26:27 -0600

    added useState to list component and modified corresponding CSS

--- src/components/list.js
+++ src/components/list.js
@@ -1,12 +1,69 @@
+import { useState } from "react";
+
 export function TodoList() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Bread",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Milk",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Juice",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  };
+
   return (
-    <ul className="todo-list">
-      <li>
-        Item 1 <button>Delete</button>
-      </li>
-      <li>
-        Item 2 <button>Delete</button>
-      </li>
-    </ul>
+    <main>
+      {items.length ? (
+        <ul className="todo-list">
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                className="item-label"
+                style={item.checked ? { textDecoration: "line-through" } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >
+                {item.item}
+              </label>
+              <button
+                className="delete-btn"
+                onClick={() => handleDelete(item.id)}
+                tabIndex="0"
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+      )}
+    </main>
   );
 }

36f9a3916a39f8107a1dc33b5182741a33bfaab4
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-28 21:21:07 -0600

    Cleanued up casing for components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,34 +1,15 @@
 import React from "react";
+import { List } from "./List";
 
 export function Content({ items, handleCheck, handleDelete }) {
   return (
     <main>
       {items.length ? (
-        <ul className="todo-list">
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                className="item-label"
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <button
-                className="delete-btn"
-                onClick={() => handleDelete(item.id)}
-                tabIndex="0"
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
+        <List
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
       )}
--- src/components/list.js
+++ src/components/list.js
@@ -1,35 +1,29 @@
-export function TodoList({ items, handleCheck, handleDelete }) {
+export function List({ items, handleCheck, handleDelete }) {
   return (
-    <main>
-      {items.length ? (
-        <ul className="todo-list">
-          {items.map((item) => (
-            <li className="item" key={item.id}>
-              <input
-                type="checkbox"
-                onChange={() => handleCheck(item.id)}
-                checked={item.checked}
-              />
-              <label
-                className="item-label"
-                style={item.checked ? { textDecoration: "line-through" } : null}
-                onDoubleClick={() => handleCheck(item.id)}
-              >
-                {item.item}
-              </label>
-              <button
-                className="delete-btn"
-                onClick={() => handleDelete(item.id)}
-                tabIndex="0"
-              >
-                Delete
-              </button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p style={{ marginTop: "2rem" }}>Your list is empty.</p>
-      )}
-    </main>
+    <ul className="todo-list">
+      {items.map((item) => (
+        <li className="item" key={item.id}>
+          <input
+            type="checkbox"
+            onChange={() => handleCheck(item.id)}
+            checked={item.checked}
+          />
+          <label
+            className="item-label"
+            style={item.checked ? { textDecoration: "line-through" } : null}
+            onDoubleClick={() => handleCheck(item.id)}
+          >
+            {item.item}
+          </label>
+          <button
+            className="delete-btn"
+            onClick={() => handleDelete(item.id)}
+            tabIndex="0"
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }

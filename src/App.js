608de133ca1e6a74141110ea0401179935a5489b
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-28 21:23:14 -0600

    Update saved App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { Header } from "./components/header";
+import { Header } from "./components/Header";
 import { Content } from "./components/Content";
-import { Footer } from "./components/footer";
+import { Footer } from "./components/Footer";
 import { useState } from "react";
 
 function App() {

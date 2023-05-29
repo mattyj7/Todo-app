5d2c4d9a07026272f2307a1e0c4e8f9184c9b7bb
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-05-29 13:04:39 -0600

    Created add item form

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,19 @@
-export function Header({ title }) {
+export function Header({ title, newItem, setNewItem, handleSubmit }) {
   return (
-    <form className="new-item-form">
+    <form className="new-item-form" onSubmit={handleSubmit}>
       <h1>{title}</h1>
       <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="itme" />
+        <label htmlFor="item">Add Item</label>
+        <input
+          autoFocus
+          type="text"
+          id="addItem"
+          placeholder="New Item"
+          required
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+        />
+        <button type="submit" aria-label="Add Item"></button>
       </div>
     </form>
   );

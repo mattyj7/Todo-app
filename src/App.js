f5ffa9ee3d89362d291190d7819d20af2bd63465
mattyj7 <120997192+mattyj7@users.noreply.github.com>
2023-06-02 12:30:43 -0600

    Added latency loading handle

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [newItem, setNewItem] = useState("");
   const [search, setSearch] = useState("");
   const [fetchError, setFetchError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -19,15 +20,18 @@ function App() {
         const response = await fetch(API_URL);
         if (!response.ok) throw Error("Did not recieve expected data");
         const listItems = await response.json();
-        console.log(listItems);
         setItems(listItems);
         setFetchError(null);
       } catch (err) {
         setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    (async () => await fetchItems())();
+    setTimeout(() => {
+      (async () => await fetchItems())();
+    }, 1000);
   }, []);
 
   const setAndSaveItems = (newItems) => {
@@ -71,8 +75,9 @@ function App() {
       />
       <SearchItem search={search} setSearch={setSearch} />
       <main>
+        {isLoading && <p>Loading Items</p>}
         {fetchError && <p>{`Error: ${fetchError}`}</p>}
-        {!fetchError && (
+        {!fetchError && !isLoading && (
           <Content
             items={items.filter((item) =>
               item.item.toLowerCase().includes(search.toLowerCase())
